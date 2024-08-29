f8c2f108f79546afc489c7da908342a87e27652b
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-08-28 21:48:15 -0300

    28/08

--- biblioteca-api/app.js
+++ biblioteca-api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const livroRoutes = require('./routes/livroRoutes');
+const cors= require ('cors');
 require('dotenv').config();
 require('./config/database'); // Conectando ao banco de dados
 
@@ -10,6 +11,8 @@ const app = express();
 
 // Middlewares
 app.use(bodyParser.json());
+
+app.use(cors());
 
 
 // Rotas
