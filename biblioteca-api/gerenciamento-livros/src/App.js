2acb61ab16d02d931bca268ff314f7342da248da
oliveiramarcelo12 <oliveiramarcelo198@gmail.com>
2024-08-29 21:55:03 -0300

    29/08

---
+++ biblioteca-api/gerenciamento-livros/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ListaLivros from './components/ListaLivro';
+import NovoLivro from './components/NovoLivro';
+import EditarLivro from './components/EditarLivro';
+import './css/style.css';
+
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<ListaLivros />} />
+                    <Route path="/novo" element={<NovoLivro />} />
+                    <Route path="/editar/:id" element={<EditarLivro />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+
+export default App;
--- biblioteca-api/server.js
+++ biblioteca-api/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 
 // Definindo a porta a partir das variáveis de ambiente ou usando o padrão 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 // Iniciar o servidor
